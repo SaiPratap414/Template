557926a47a5bfe92b61c50ec52f97d93d3aa885d
Sai Pratap <pratap@illiquidlabs.com>
2024-09-02 10:17:48 +0530

    icon update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,31 @@
+// Sidebar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FiMail, FiBell, FiSettings } from 'react-icons/fi'; // Importing icons from React Icons
 
 function Sidebar() {
   return (
     <nav className="sidebar">
-      <div className="logo">Logo</div>
+      {/* Wrap the logo in a Link to navigate to the default route */}
+      <Link to="/" className="logo">
+        Logo
+      </Link>
       <ul>
-        <li><Link to="/">Email Template</Link></li>
-        <li><Link to="/notification-templates">Notification Template</Link></li>
-        <li><Link to="/settings">Settings</Link></li>
+        <li>
+          <Link to="/">
+            <FiMail className="icon" /> Email Template
+          </Link>
+        </li>
+        <li>
+          <Link to="/notification-templates">
+            <FiBell className="icon" /> Notification Template
+          </Link>
+        </li>
+        <li>
+          <Link to="/settings">
+            <FiSettings className="icon" /> Settings
+          </Link>
+        </li>
       </ul>
     </nav>
   );
